6c5f503d41156eb0b7b4c39e51a6a97bc3017498
Faith Schneider <niahcx@yahoo.com>
2017-04-07 17:11:14 -0600

    compile less

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,21 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	less = require('gulp-less')
+;
 
+// Compile Less
+gulp.task('styles', function() {
+	gulp.src('less/**/*.less')
+	.pipe(less({
+		filename: 'styles.less',
+		compress: true
+	}))
+	.pipe(gulp.dest('faithbot/_css/'));
+});
 
-// gulp.task('default', ['scripts', 'styles', 'watch']);
+// Watch file changes
+gulp.task('watch', function(){
+	gulp.watch('less/**/*.less', ['styles']);
+});
+
+// Default task
+gulp.task('default', ['styles', 'watch']);
